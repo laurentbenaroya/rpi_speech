c89e5e1953c24bc4bbbfb4edbc47ec4ad7991308
benaroya <laurent.benaroya@gmail.com>
2022-10-25 00:04:42 +0200

    first commit

---
+++ distilgpt2.py
@@ -0,0 +1,84 @@
+"""
+distil-gpt2 next sentence generation (run on raspberrypi 3B+, 1Go RAM)
+https://dejanbatanjac.github.io/gpt2-example/
+https://medium.com/analytics-vidhya/ai-writer-text-generation-using-gpt-2-transformers-4c33d8c52d5a
+E.L. Benaroya - 10/2022
+"""
+
+
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import torch
+
+import time
+import random
+import numpy as np
+import os
+import sys
+
+
+def gensuite():
+    sentence=input("Enter your sentence : ")
+    print(f' sentence type : {type(sentence)}')
+    mode = input("Enter gen mode : [d/s]")
+    if len(mode) == 0:
+        mode = 'd'
+    else:
+        mode = mode.lower()
+    # sentence = "What is love?"
+    print(sentence)
+    print(mode)
+
+    input_ids = tokenizer.encode(sentence)
+    print(f'input ids {input_ids}')
+    # print(tokenizer.decode(input_ids[0]))
+
+    tic = time.time()
+    print('generate text')
+    model.eval()
+    if mode == 'd':
+        print('deterministic')
+        outputs = model.generate(torch.tensor(input_ids).unsqueeze(0),
+            max_length = 10000,
+            num_beams = 5,
+            no_repeat_ngram_size  = 2,
+            early_stopping = True)
+    else:
+        print('sampling')
+        outputs = model.generate(
+            torch.tensor(input_ids).unsqueeze(0), 
+            max_length=500,
+            do_sample=True,
+            top_k=20,
+            temperature=0.7
+            )
+    print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+    print(f'outputs shape : {outputs.shape}')  # (torch.Size([1, 500]), torch.Size([500]))
+    print(f'Elapsed time = {(time.time()-tic):.1f}')
+
+
+print('load tokenizer')
+tokenizer = GPT2Tokenizer.from_pretrained("distilgpt2")
+
+print('load model')
+tic = time.time()
+model = GPT2LMHeadModel.from_pretrained("distilgpt2", pad_token_id=tokenizer.eos_token_id)
+print(f'Elapsed time : {(time.time()-tic):.1f}')
+
+print(f'num parameters in model = {sum(p.numel() for p in model.parameters() if p.requires_grad)}')
+
+print('set random seed')
+seed = 0  # random.randint(0, 13)
+np.random.seed(seed)
+torch.random.manual_seed(seed)
+# torch.cuda.manual_seed(seed)
+device = "cpu"  # torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+print('display memory state')
+os.system('sudo free -h')
+
+# run inference
+while True:
+    try:
+        gensuite()
+    except KeyboardInterrupt:
+        sys.exit()
---
+++ speech2text_wav2vec2.py
@@ -0,0 +1,51 @@
+import os
+import time
+import soundfile as snd
+import torch
+import sys
+
+from transformers import Wav2Vec2Processor
+from transformers import Wav2Vec2ForCTC
+
+
+print(torch.__version__)
+torch.random.manual_seed(0)
+device = 'cpu'
+
+SPEECH_FILE = sys.argv[1]
+waveform, sample_rate = snd.read(SPEECH_FILE)
+
+# from hugging face hub
+remote_model_path = 'facebook/wav2vec2-base-960h'
+print(remote_model_path)
+
+# load processor
+print('load processor from hub')
+processor = Wav2Vec2Processor.from_pretrained(remote_model_path)
+
+# load model from hub
+print('load model from hub')
+tic = time.time()
+model = Wav2Vec2ForCTC.from_pretrained(remote_model_path)
+print(f'Elapsed time {(time.time()-tic):0f}')
+
+print('tokenize')
+tic = time.time()
+input_values = processor(waveform, return_tensors="pt", padding="longest", sampling_rate=sample_rate).input_values  # Batch size 1
+print(input_values.shape)
+print(f'Elapsed time {(time.time()-tic):0f}')
+
+# retrieve logits
+print('retrieve logits')
+tic = time.time()
+logits = model(input_values).logits
+print(f'Elapsed time {(time.time()-tic):0f}')
+
+# take argmax and decode
+print('take argmax and decode')
+tic = time.time()
+predicted_ids = torch.argmax(logits, dim=-1)
+transcription = processor.batch_decode(predicted_ids)
+print(f'Elapsed time {(time.time()-tic):0f}')
+
+print(transcription)
